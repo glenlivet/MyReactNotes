a6cc6259305851d68529a153d3b3f09e7cef102c
Glenlivet Zhang <glenlivet.zhang@pwc.com>
2016-04-29 00:58:17 +0800

    styling corrected.
    styling corrected.

--- App/Components/NoteScreen.js
+++ App/Components/NoteScreen.js
@@ -2,6 +2,7 @@
 import React, {
   StyleSheet,
   TextInput,
+  Text,
   View
 } from 'react-native';
 
@@ -9,23 +10,23 @@ export default class NoteScreen extends React.Component {
   render () {
     return (
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder="Untitled" 
-          ref="title" 
-          autoFocus={true}
-          autoCapitalize="sentences"
-          style={[styles.textInput,styles.title]} 
-          onEndEditing={(text)=>{this.refs.body.focus()}}
-        />
-        </View>
-        <View style={styles.inputContainer}>
-        <TextInput 
-          ref="body" 
-          multiline={true} 
-          placeholder="Start typing..." 
-          style={[styles.body, styles.textInput]} 
-        />
+      <View style={styles.inputContainer}>
+          <TextInput 
+            placeholder="Untitled" 
+            ref="title" 
+            autoFocus={true}
+            autoCapitalize="sentences"
+            style={[styles.textInput, styles.title]} 
+            onEndEditing={(text)=>{this.refs.body.focus()}}
+          />
+          </View>
+        <View style={[styles.inputContainer, styles.body]}>
+          <TextInput 
+            ref="body" 
+            multiline={true} 
+            placeholder="Start typing..." 
+            style={[styles.textInput, styles.body]} 
+          />
         </View>
       </View>
     );
@@ -37,7 +38,8 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 64
+    marginTop: 64,
+    marginHorizontal: 20
   },
   title: {
     height: 40
@@ -50,6 +52,7 @@ var styles = StyleSheet.create({
     fontSize: 16
   },
   inputContainer: {
+    alignSelf: 'stretch',
     borderBottomColor: '#9E7CE3',
     borderBottomWidth: 1,
     flexDirection: 'row',
